1197f5a26de60856a9c2f8f2255628e6689062da
maeii426 <mar.anne.369@gmail.com>
2019-03-23 02:27:00 +0000

    Preliminary login and create user page

--- football_bingo/config/routes.rb
+++ football_bingo/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   root 'static_pages#home'
