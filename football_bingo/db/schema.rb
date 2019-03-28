78248f7aab48844eea72f31eec415aeb24a11677
Ying <ying.wang@tamu.edu>
2019-03-28 01:54:25 +0000

    minor change in the home page: don't show sign up when logged in

--- football_bingo/db/schema.rb
+++ football_bingo/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20190324032543) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string   "gameid"
     t.datetime "date"
@@ -28,6 +31,6 @@ ActiveRecord::Schema.define(version: 20190324032543) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
