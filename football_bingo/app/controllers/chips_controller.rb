2146f6be447f34febd180bcbc037dcb396c08d98
maeii426 <mar.anne.369@gmail.com>
2019-03-29 11:42:29 +0000

    basic parsing

--- football_bingo/app/controllers/chips_controller.rb
+++ football_bingo/app/controllers/chips_controller.rb
@@ -1,6 +1,4 @@
 class ChipsController < ApplicationController
 
-    def self.translate
-        return conditional + argument + value.to_s
-    end
+    
 end
--- football_bingo/app/models/chip.rb
+++ football_bingo/app/models/chip.rb
@@ -1,5 +1,32 @@
 class Chip < ActiveRecord::Base
     def translate
-        return conditional + argument + value.to_s
+        if check
+            return conditional + argument + value.to_s + ' ' + check.to_s
+        end
+        
+        return conditional + argument + value.to_s + ' ' + check.to_s
+    end
+    
+    def check
+       # Fetch and parse HTML document
+        doc = Nokogiri::XML(open(Rails.root + 'XML/tam.xml'))    #('https://nokogiri.org/tutorials/installing_nokogiri.html'))
+ 
+        # ASSUMPTIONS
+            # no '_' tokens in the XML tag
+            # right now - tags that should have only one instance are assumed to have only one instance
+            # ********* CONDITION IS >
+        tokens = conditional.split('_', 2)
+        doc_content = doc.at(tokens[0])
+        if doc_content
+            entries = doc_content.to_s
+            if entries.include?(tokens[1])
+                doc_value = entries.match(/#{tokens[1]}="([^"]*)"/).captures
+                #doc_value = entries.match(/(?<#{tokens[1]}>\w+)/)
+                if value <= doc_value[0].to_f
+                    return true
+                end
+            end
+        end
+        false
     end
 end
