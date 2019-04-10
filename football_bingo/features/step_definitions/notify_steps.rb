1cfe184c08e53a5d66e5f9636cb9d33110fe4aca
Xiaojing Yu <xiaojingyu92@gmail.com>
2019-04-10 02:30:33 -0500

    add sending email button in admin mode

---
+++ football_bingo/features/step_definitions/notify_steps.rb
@@ -0,0 +1,34 @@
+Then("I should see {string}") do |string|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then("I press {string}") do |string|
+  click_button(string)
+end
+
+When("I press {string} at {string}") do |string, string2|
+  visit path_to('the send page')
+  game = Game.find_by(game_name: string2)
+  a=send_email_path()
+end
+
+When("I follow {string}") do |string|
+  click_link(string)
+end
+
+Given("I am the administrator") do
+  User.create!({:name => 'admin',
+                :password => '123456',
+                :email => 'admin@gmail.com',
+                :role => 'admin'})
+
+  visit '/login'
+  fill_in 'Email', :with => 'admin@gmail.com'
+  fill_in 'Password', :with => '123456'
+  click_button 'Log in'
+
+end
