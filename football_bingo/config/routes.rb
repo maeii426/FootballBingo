3641623e0640bd584dcb3801214730265c6a29ce
damonwy <ying.wang@tamu.edu>
2019-04-29 22:02:35 -0500

    accidently deleted routes..

---
+++ football_bingo/config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get  '/help' => 'static_pages#help'
+  get  '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/score_board' => 'games#score_board'
+  post '/send_email' => 'games#send_email'
+  post '/games' => :show, controller: 'games'
+
+  resources :users do
+    get '/share' => 'users#share'
+    resources :games do
+      get '/play' => 'games#play_game'
+      post '/join' => 'games#join'
+      post '/get_new_card' => 'games#get_new_card'
+      post '/get_whoop_card' => 'games#get_whoop_card'
+      post '/check_win' => 'users#check_win'
+    end
+  end
+
+  root 'static_pages#home'
+  #root 'users#index'
+  resources :games do
+
+  end
+end
