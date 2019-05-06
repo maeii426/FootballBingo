37345c413ecd8cc6b6d73843a1d9517ad1a38a23
Xiaojing Yu <xiaojingyu92@gmail.com>
2019-05-06 17:49:40 -0500

    run rspec and cucumber in local & generate coverage report

--- football_bingo/spec/lib/import_xml_spec.rb
+++ football_bingo/spec/lib/import_xml_spec.rb
@@ -6,8 +6,10 @@ describe ImportXML do
 
     describe "parse xml correct" do
       it "should have name" do
+
         get_xml_game("./XML/tam.xml")
         update_xml_game("./XML/tam.xml")  
+
       end
     end
 
